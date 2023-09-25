174e55cf88dd9b4aeaeed820235526efd7632cd8
pooya_dehghan <pooyadehghan@yahoo.com>
2023-09-25 17:13:41 +0330

    attached leaderboard microservice to apigateway

--- apps/apigateway/src/leaderboard/leaderboard.controller.ts
+++ apps/apigateway/src/leaderboard/leaderboard.controller.ts
@@ -1,4 +1,36 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  UseInterceptors,
+  UseFilters,
+  Post,
+  Get,
+  Body,
+  Param,
+  ParseIntPipe,
+  UseGuards,
+} from '@nestjs/common';
+import { LeaderboardService } from './leaderboard.service';
+import { ApiTags, ApiBasicAuth } from '@nestjs/swagger';
+import { JwtGuard } from '../auth/guard';
+import { CreateLeaderBoardDto, FindLeaderBoardDto } from './dto';
 
 @Controller('leaderboard')
-export class LeaderboardController {}
+export class LeaderboardController {
+  constructor(private readonly leaderboardService: LeaderboardService) {}
+
+  @ApiBasicAuth()
+  @UseGuards(JwtGuard)
+  @Post('create')
+  async createLeaderBoard(@Body() payload: CreateLeaderBoardDto) {
+    return this.leaderboardService.createLeaderBoard(payload);
+  }
+
+  @ApiBasicAuth()
+  @UseGuards(JwtGuard)
+  @Get('/:leaderboardID')
+  async finOneLeaderBoard(
+    @Param('leaderboardID', ParseIntPipe) payload: number,
+  ) {
+    return this.leaderboardService.findLeaderBoard(payload);
+  }
+}
